2cfe3cf3a34a1b77d3ba9b40fde1c1634ef2138e
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-12-08 21:40:22 +0300

    fix 5

--- models.py
+++ models.py
@@ -1,24 +1,24 @@
-# from sqlalchemy.sql import case
-# from flask_migrate import Migrate
-# import dateutil.parser
-# import babel
-# from flask import Flask, render_template, request, Response, flash, redirect, url_for
-# from flask_moment import Moment
-# import os
-# from sqlalchemy import Column, String, Integer
-# from flask_sqlalchemy import SQLAlchemy
-# import json
-# import datetime
-# import os
+from sqlalchemy.sql import case
+from flask_migrate import Migrate
+import dateutil.parser
+import babel
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+import datetime
+import os
 
 
-# SECRET_KEY = os.urandom(32)
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_KEY = os.urandom(32)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # DEBUG = True
