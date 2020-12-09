4310d426221dd7051bced551b41bcff7e95bdd9f
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2020-12-09 14:47:44 +0300

    added bash file

--- auth.py
+++ auth.py
@@ -3,13 +3,13 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-
+import os
 from jose.constants import Algorithms
 
 
-AUTH0_DOMAIN = 'heilafsnd.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'Movies'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 
 class AuthError(Exception):
